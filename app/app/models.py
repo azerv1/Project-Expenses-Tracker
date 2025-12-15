da9649cca651aa205d91c959594ffb1cfc35ec55
azerv1 <asterioszer@gmail.com>
2025-12-15 17:19:07 +0200

    feat: add validation logic for expenseitem.price and employee.age

--- app/app/models.py
+++ app/app/models.py
@@ -1,11 +1,12 @@
 import uuid
 from django.db import models
+from django.core.validators import MinValueValidator
 from auditlog.registry import auditlog
 
         
 class Employee(models.Model):
     name = models.CharField(max_length=100)
-    age = models.IntegerField()
+    age = models.IntegerField(validators=[MinValueValidator(18)])
     
     def __str__(self):
         return self.name
@@ -28,7 +29,7 @@ class Receipt(models.Model):
 
     @property
     def total(self):
-        return sum(item.price * (1+ item.VAT/100) for item in self.items.all())
+        return sum(item.price * (1+ item.VAT/100) * item.quantity for item in self.items.all())
 
     def __str__(self):
         return f"{self.employee}: {self.total}$"
@@ -36,8 +37,9 @@ class Receipt(models.Model):
 class ExpenseItem(models.Model):
     receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE, related_name='items')
     item = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10,decimal_places=2)
-    VAT = models.DecimalField(max_digits=4,decimal_places=2) # 24% 14% etc, not 0.24
+    price = models.DecimalField(max_digits=10,decimal_places=2, validators=[MinValueValidator(0)])
+    VAT = models.DecimalField(max_digits=4,decimal_places=2,validators=[MinValueValidator(0)]) # 24% 14% etc, not 0.24
+    quantity = models.IntegerField(validators=[MinValueValidator(0)])
 
     def __str__(self):
         return f"{self.item}: {self.price}$, {self.VAT}% VAT"
